e3f243112173dab4c238f20d5ece760b4492834b
saber2pr <saber2pr@gmail.com>
2023-12-19 13:39:42 +0800

    chore: update

--- sitemap.js
+++ sitemap.js
@@ -1,15 +1,18 @@
 const moment = require('moment')
 
-const createSitemap = (cname = '', basename = '', urls) => `<?xml version="1.0" encoding="utf-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
+const createSitemap = (cname = '', basename = '', urls) => `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 <url>
     <loc>https://${cname}/</loc>
-    <priority>1.00</priority>
+    <lastmod>${moment().format('YYYY-MM-DD')}</lastmod>
+    <changefreq>daily</changefreq>
+    <priority>0.6</priority>
 </url>
   ${urls.map(url => `<url>
   <loc>${'https://' + cname + basename + url}</loc>
   <lastmod>${moment().format('YYYY-MM-DD')}</lastmod>
   <changefreq>daily</changefreq>
+  <priority>0.6</priority>
 </url>`).join('\n')}
 </urlset>`
 
